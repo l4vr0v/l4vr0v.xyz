6cc27fa43fe3891be88cc37d11942c7865eb1e2c
l4vr0v <l4vr0v@l4vr0v.xyz>
2020-11-17 00:07:21 -0800

    lipsum in Home Page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-
+    <div class="hero is-cover is-relative is-fullheight-with-navbar is-primary">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title is-1">lorem ipsum.</h1>
+                <h2 class="subtitle is-3">dolor sit amet.</h2>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
