72a691c74d7bf8674fa30809ad096b139251cbab
l4vr0v <l4vr0v@l4vr0v.xyz>
2020-11-19 00:38:56 -0800

    fix and integrate router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import VueSimpleMarkdown from 'vue-simple-markdown'
 import 'vue-simple-markdown/dist/vue-simple-markdown.css'
 
@@ -8,5 +9,6 @@ Vue.use(VueSimpleMarkdown)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
